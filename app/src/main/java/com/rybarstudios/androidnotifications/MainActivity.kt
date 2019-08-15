603fc19794d119cf2ae76f4937d0a2181ff140c3
Christian Rybarczyk <christian.j.rybarczyk@gmail.com>
2019-08-15 16:26:51 -0400

    Built notification generator

--- app/src/main/java/com/rybarstudios/androidnotifications/MainActivity.kt
+++ app/src/main/java/com/rybarstudios/androidnotifications/MainActivity.kt
@@ -2,11 +2,21 @@ package com.rybarstudios.androidnotifications
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rybarstudios.androidnotifications.util.NotificationGenerator.getNotification
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val NOTIFICATION_ID = 42
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_get_notification.setOnClickListener {
+            getNotification(this)
+        }
     }
 }
--- app/src/main/java/com/rybarstudios/androidnotifications/util/NotificationGenerator.kt
+++ app/src/main/java/com/rybarstudios/androidnotifications/util/NotificationGenerator.kt
@@ -1,2 +1,41 @@
 package com.rybarstudios.androidnotifications.util
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import com.rybarstudios.androidnotifications.MainActivity
+import com.rybarstudios.androidnotifications.R
+
+object NotificationGenerator {
+
+    fun getNotification(context: Context) {
+
+
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val channelId = "${context.packageName}.simplenotification"
+
+        //Create required notification channel for versions O and above
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Notification channel"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val description = "Notification description goes here"
+
+            val channel = NotificationChannel(channelId, name, importance)
+            channel.description = description
+
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val builder = NotificationCompat.Builder(context, channelId)
+            .setPriority(NotificationManager.IMPORTANCE_DEFAULT)
+            .setContentTitle("Content title goes here")
+            .setSmallIcon(android.R.drawable.ic_media_play)
+            .setColor(context.resources.getColor(R.color.colorAccent))
+            .setDefaults(Notification.DEFAULT_ALL)
+        notificationManager.notify(MainActivity.NOTIFICATION_ID, builder.build())
+    }
+}
